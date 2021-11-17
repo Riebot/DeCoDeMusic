d0c16ec2b392324f9e6cfc2e04ae24ce67829c2c
Riebot <86209111+Riebot@users.noreply.github.com>
2021-11-17 22:00:19 +0700

    Update admins.py

--- plugins/admins.py
+++ plugins/admins.py
@@ -13,15 +13,14 @@ from Client import callsmusic
 from pytgcalls.types.input_stream import InputAudioStream
 from pytgcalls.types.input_stream import InputStream
 
-
 @Client.on_message(command(["pause", "jeda"]) & other_filters)
 @errors
 @authorized_users_only
 async def pause(_, message: Message):
     await callsmusic.pytgcalls.pause_stream(message.chat.id)
     await message.reply_photo(
-                             photo="https://telegra.ph/file/dd6814e241bfc4c0255cd.jpg", 
-                             caption="**⏸ Music Paused.\n use /resume**"
+                             photo="https://telegra.ph/file/e6443c3ba9f2cc48f5fa3.jpg", 
+                             caption="**⏸ 𝐌𝐮𝐬𝐢𝐜 𝐝𝐢𝐡𝐞𝐧𝐭𝐢𝐤𝐚𝐧 𝐬𝐞𝐦𝐞𝐧𝐭𝐚𝐫𝐚\n 𝐤𝐞𝐭𝐢𝐤 /resume 𝐮𝐧𝐭𝐮𝐤 𝐦𝐞𝐥𝐚𝐧𝐣𝐮𝐭𝐤𝐚𝐧 𝐦𝐮𝐬𝐢𝐜**"
     )
 
 
@@ -31,8 +30,8 @@ async def pause(_, message: Message):
 async def resume(_, message: Message):
     await callsmusic.pytgcalls.resume_stream(message.chat.id)
     await message.reply_photo(
-                             photo="https://telegra.ph/file/d0f2dd5b7519bb5444139.jpg", 
-                             caption="**▶️ Music Resumed.\n use /pause**"
+                             photo="https://telegra.ph/file/126ebe97a5f318a67e24a.jpg", 
+                             caption="**▶️ 𝐌𝐮𝐬𝐢𝐜 𝐝𝐢𝐥𝐚𝐧𝐣𝐮𝐭𝐤𝐚𝐧\n 𝐤𝐞𝐭𝐢𝐤 /pause 𝐮𝐧𝐭𝐮𝐤 𝐦𝐞𝐧𝐣𝐞𝐝𝐚 𝐦𝐮𝐬𝐢𝐜**"
     )
 
 
@@ -47,44 +46,41 @@ async def stop(_, message: Message):
 
     await callsmusic.pytgcalls.leave_group_call(message.chat.id)
     await message.reply_photo(
-                             photo="https://telegra.ph/file/8d22aa7d53b6acb9a125e.jpg", 
-                             caption="❌ **Stopped Streaming\n use /play for new song**"
+                             photo="https://telegra.ph/file/ca39c6b4904288d69a6d9.jpg", 
+                             caption="**❌ 𝐌𝐮𝐬𝐢𝐜 𝐭𝐞𝐥𝐚𝐡 𝐝𝐢𝐦𝐚𝐭𝐢𝐤𝐚𝐧 ❌\n 𝐤𝐞𝐭𝐢𝐤 /play 𝐮𝐧𝐭𝐮𝐤 𝐦𝐞𝐦𝐮𝐥𝐚𝐢𝐧𝐲𝐚 𝐥𝐚𝐠𝐢**"
     )
 
-@Client.on_message(command(["skip", "second", "next", f"next@{BOT_USERNAME}"]) & other_filters)
+
+@Client.on_message(command(["skip", "next"]) & other_filters)
 @errors
 @authorized_users_only
 async def skip(_, message: Message):
     global que
     chat_id = message.chat.id
-    ACTV_CALLS = {}
+    ACTV_CALL = []
     for x in callsmusic.pytgcalls.active_calls:
-        ACTV_CALLS(int(x.chat_id))
-    if int(chat_id) not in ACTV_CALLS:
-        await message.reply_text("❌ **no music is currently playing**")
+        ACTV_CALL.append(int(x.chat_id))
+    if int(chat_id) not in ACTV_CALL:
+        await message.reply_text("❎ 𝐓𝐢𝐝𝐚𝐤 𝐚𝐝𝐚 𝐥𝐚𝐠𝐮 𝐲𝐚𝐧𝐠 𝐝𝐢 𝐬𝐤𝐢𝐩!")
     else:
-        queues.task_done(chat_id)
-        
-        if queues.is_empty(chat_id):
+        callsmusic.queues.task_done(chat_id)
+
+        if callsmusic.queues.is_empty(chat_id):
             await callsmusic.pytgcalls.leave_group_call(chat_id)
         else:
             await callsmusic.pytgcalls.change_stream(
-                chat_id, 
-                InputStream(
-                    InputAudioStream(
-                        callsmusic.queues.get(chat_id)["file"],
-                    ),
-                ),
+                chat_id, InputAudioStream(callsmusic.queues.get(chat_id)["file"])
             )
-                
+
     qeue = que.get(chat_id)
     if qeue:
-        qeue.pop(0)
+        skip = qeue.pop(0)
     if not qeue:
         return
-    await message.reply_text("⏭ **You've skipped to the next song.**")
-
-
+    await message.reply_photo(
+                             photo="https://telegra.ph/file/96129f4d0e984d2432e55.jpg", 
+                             caption=f'- Skipped **{skip[0]}**\n- Now Playing **{qeue[0][0]}**'
+   ) 
 
 
 @Client.on_message(filters.command(["reload", "refresh"]))
@@ -101,5 +97,5 @@ async def admincache(client, message: Message):
 
     await message.reply_photo(
                               photo="https://telegra.ph/file/d881ea9de7620ecc36d08.jpg",
-                              caption="**Reloaded\n Admin List updated**"
+                              caption="**𝐃𝐚𝐟𝐭𝐚𝐫 𝐚𝐝𝐦𝐢𝐧 𝐭𝐞𝐥𝐚𝐡 𝐝𝐢𝐩𝐞𝐫𝐛𝐚𝐫𝐮𝐢!**"
     )
