1f27ba99d11379fe7f9f16f8b5abac55496b27a7
Riebot <86209111+Riebot@users.noreply.github.com>
2021-11-25 03:28:23 +0700

    Okjing

--- DeCalls/DeCalls.py
+++ DeCalls/DeCalls.py
@@ -1,30 +1,27 @@
-import sira
 import config
+import sira
 from pyrogram import Client
 from pytgcalls import PyTgCalls
 from pytgcalls.types import Update
 from pytgcalls.types.input_stream import InputAudioStream
 
-
-
 client = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
 pytgcalls = PyTgCalls(client)
 
-
 @pytgcalls.on_stream_end()
-async def on_stream_end(client: PyTgCalls, update: Update) -> None:
+async def on_stream_end(_, update: Update):
     chat_id = update.chat_id
-    queues.task_done(chat_id)
+    sira.task_done(chat_id)
 
-    if queues.is_empty(chat_id):
+if sira.is_empty(chat_id):
         await pytgcalls.leave_group_call(chat_id)
     else:
         await pytgcalls.change_stream(
-            chat_id, 
-            InputAudioStream(
-                queues.get(chat_id)["file"],
+            chat_id,
+            InputStream(
+                InputAudioStream(
+                    sira.get(u.chat_id)["file"],
+                ),
             ),
         )
 
-
-run = pytgcalls.start
