a1d8ef547d4e16d767edb8d6e5cf2855943605a1
Tofik Denianto <77754555+tofikdn@users.noreply.github.com>
2021-11-25 04:35:20 +0700

    Update admins.py

--- plugins/admins.py
+++ plugins/admins.py
@@ -1,16 +1,13 @@
 from asyncio.queues import QueueEmpty
-from config import BOT_USERNAME
-from config import que
+
+from cache.admins import set
+from Client import callsmusic
+from helpers.decorators import authorized_users_only, errors
+from helpers.filters import command, other_filters
 from pyrogram import Client, filters
 from pyrogram.types import Message
-import sira
-import DeCalls
-from cache.admins import set
-from helpers.decorators import authorized_users_only, errors
-from helpers.channelmusic import get_chat_id
-from helpers.filters import command, other_filters
-from Client import callsmusic
-from pytgcalls.types.input_stream import InputAudioStream
+from pytgcalls.types.input_stream import InputAudioStream, InputStream
+
 
 @Client.on_message(command(["pause", "jeda"]) & other_filters)
 @errors
@@ -18,8 +15,8 @@ from pytgcalls.types.input_stream import InputAudioStream
 async def pause(_, message: Message):
     await callsmusic.pytgcalls.pause_stream(message.chat.id)
     await message.reply_photo(
-                             photo="https://telegra.ph/file/e6443c3ba9f2cc48f5fa3.jpg", 
-                             caption="**⏸ 𝐌𝐮𝐬𝐢𝐜 𝐝𝐢𝐡𝐞𝐧𝐭𝐢𝐤𝐚𝐧 𝐬𝐞𝐦𝐞𝐧𝐭𝐚𝐫𝐚\n 𝐤𝐞𝐭𝐢𝐤 /resume 𝐮𝐧𝐭𝐮𝐤 𝐦𝐞𝐥𝐚𝐧𝐣𝐮𝐭𝐤𝐚𝐧 𝐦𝐮𝐬𝐢𝐜**"
+        photo="https://telegra.ph/file/e6443c3ba9f2cc48f5fa3.jpg",
+        caption="**⏸ 𝐌𝐮𝐬𝐢𝐜 𝐝𝐢𝐡𝐞𝐧𝐭𝐢𝐤𝐚𝐧 𝐬𝐞𝐦𝐞𝐧𝐭𝐚𝐫𝐚\n 𝐤𝐞𝐭𝐢𝐤 /resume 𝐮𝐧𝐭𝐮𝐤 𝐦𝐞𝐥𝐚𝐧𝐣𝐮𝐭𝐤𝐚𝐧 𝐦𝐮𝐬𝐢𝐜**",
     )
 
 
@@ -29,8 +26,8 @@ async def pause(_, message: Message):
 async def resume(_, message: Message):
     await callsmusic.pytgcalls.resume_stream(message.chat.id)
     await message.reply_photo(
-                             photo="https://telegra.ph/file/126ebe97a5f318a67e24a.jpg", 
-                             caption="**▶️ 𝐌𝐮𝐬𝐢𝐜 𝐝𝐢𝐥𝐚𝐧𝐣𝐮𝐭𝐤𝐚𝐧\n 𝐤𝐞𝐭𝐢𝐤 /pause 𝐮𝐧𝐭𝐮𝐤 𝐦𝐞𝐧𝐣𝐞𝐝𝐚 𝐦𝐮𝐬𝐢𝐜**"
+        photo="https://telegra.ph/file/126ebe97a5f318a67e24a.jpg",
+        caption="**▶️ 𝐌𝐮𝐬𝐢𝐜 𝐝𝐢𝐥𝐚𝐧𝐣𝐮𝐭𝐤𝐚𝐧\n 𝐤𝐞𝐭𝐢𝐤 /pause 𝐮𝐧𝐭𝐮𝐤 𝐦𝐞𝐧𝐣𝐞𝐝𝐚 𝐦𝐮𝐬𝐢𝐜**",
     )
 
 
@@ -45,8 +42,8 @@ async def stop(_, message: Message):
 
     await callsmusic.pytgcalls.leave_group_call(message.chat.id)
     await message.reply_photo(
-                             photo="https://telegra.ph/file/ca39c6b4904288d69a6d9.jpg", 
-                             caption="**❌ 𝐌𝐮𝐬𝐢𝐜 𝐭𝐞𝐥𝐚𝐡 𝐝𝐢𝐦𝐚𝐭𝐢𝐤𝐚𝐧 ❌\n 𝐤𝐞𝐭𝐢𝐤 /play 𝐮𝐧𝐭𝐮𝐤 𝐦𝐞𝐦𝐮𝐥𝐚𝐢𝐧𝐲𝐚 𝐥𝐚𝐠𝐢**"
+        photo="https://telegra.ph/file/ca39c6b4904288d69a6d9.jpg",
+        caption="**❌ 𝐌𝐮𝐬𝐢𝐜 𝐭𝐞𝐥𝐚𝐡 𝐝𝐢𝐦𝐚𝐭𝐢𝐤𝐚𝐧 ❌\n 𝐤𝐞𝐭𝐢𝐤 /play 𝐮𝐧𝐭𝐮𝐤 𝐦𝐞𝐦𝐮𝐥𝐚𝐢𝐧𝐲𝐚 𝐥𝐚𝐠𝐢**",
     )
 
 
@@ -68,7 +65,12 @@ async def skip(_, message: Message):
             await callsmusic.pytgcalls.leave_group_call(chat_id)
         else:
             await callsmusic.pytgcalls.change_stream(
-                chat_id, InputAudioStream(callsmusic.sira.get(chat_id)["file"])
+                chat_id,
+                InputStream(
+                    InputAudioStream(
+                        queues.get(chat_id)["file"],
+                    ),
+                ),
             )
 
     qeue = que.get(chat_id)
@@ -92,6 +94,6 @@ async def admincache(client, message: Message):
     )
 
     await message.reply_photo(
-                              photo="https://telegra.ph/file/d881ea9de7620ecc36d08.jpg",
-                              caption="**𝐃𝐚𝐟𝐭𝐚𝐫 𝐚𝐝𝐦𝐢𝐧 𝐭𝐞𝐥𝐚𝐡 𝐝𝐢𝐩𝐞𝐫𝐛𝐚𝐫𝐮𝐢!**"
+        photo="https://telegra.ph/file/d881ea9de7620ecc36d08.jpg",
+        caption="**𝐃𝐚𝐟𝐭𝐚𝐫 𝐚𝐝𝐦𝐢𝐧 𝐭𝐞𝐥𝐚𝐡 𝐝𝐢𝐩𝐞𝐫𝐛𝐚𝐫𝐮𝐢!**",
     )
