b53f88d7ae8ee4379902e03f634632355cb3633a
Tofik Denianto <77754555+tofikdn@users.noreply.github.com>
2021-11-25 04:26:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import requests
-from pyrogram import idle
-from pyrogram import Client as Bot
-
 from Client.callsmusic import run
-from config import API_ID, API_HASH, BOT_TOKEN, BG_IMAGE
+from config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
+from pyrogram import Client as Bot
+from pytgcalls import idle
 
 response = requests.get(BG_IMAGE)
 with open("./etc/foreground.png", "wb") as file:
@@ -11,13 +10,9 @@ with open("./etc/foreground.png", "wb") as file:
 
 
 bot = Bot(
-    ":memory:",
-    API_ID,
-    API_HASH,
-    bot_token=BOT_TOKEN,
-    plugins=dict(root="plugins")
+    ":memory:", API_ID, API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="plugins")
 )
 
 bot.start()
 run()
-idle() 
+idle()
