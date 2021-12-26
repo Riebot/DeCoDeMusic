02d8651536bf11e7d92de8e816b6eabeaa7b08a0
Riebot <86209111+Riebot@users.noreply.github.com>
2021-12-26 23:31:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import requests
-from Client.callsmusic import run
-from config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
-from pyrogram import Client as Bot
 from pytgcalls import idle
+from pyrogram import Client as Bot
+
+from Client.callsmusic import run
+from config import API_ID, API_HASH, BOT_TOKEN, BG_IMAGE
 
 response = requests.get(BG_IMAGE)
 with open("./etc/foreground.png", "wb") as file:
     file.write(response.content)
-
-
 bot = Bot(
-    ":memory:", API_ID, API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="plugins")
+    ":memory:",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="plugins")
 )
 
 bot.start()
