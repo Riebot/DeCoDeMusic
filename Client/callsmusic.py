65cbe3787d423d217abfbed371258e83c608e552
Riebot <86209111+Riebot@users.noreply.github.com>
2021-12-26 23:41:24 +0700

    Update callsmusic.py

--- Client/callsmusic.py
+++ Client/callsmusic.py
@@ -1,17 +1,18 @@
-import config
 from pyrogram import Client
 from pytgcalls import PyTgCalls
 from pytgcalls.types import Update
-from pytgcalls.types.input_stream import InputAudioStream, InputStream
+from pytgcalls.types.input_stream import InputStream
+from pytgcalls.types.input_stream import InputAudioStream
+
+from config import API_HASH, API_ID, SESSION_NAME
 
 from . import queues
 
-client = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
+client = Client(SESSION_NAME, API_ID, API_HASH)
 pytgcalls = PyTgCalls(client)
 
-
 @pytgcalls.on_stream_end()
-async def on_stream_end(_, update: Update):
+async def on_stream_end(client: PyTgCalls, update: Update) -> None:
     chat_id = update.chat_id
     queues.task_done(chat_id)
 
@@ -19,13 +20,12 @@ async def on_stream_end(_, update: Update):
         await pytgcalls.leave_group_call(chat_id)
     else:
         await pytgcalls.change_stream(
-            chat_id,
+            chat_id, 
             InputStream(
                 InputAudioStream(
                     queues.get(chat_id)["file"],
                 ),
             ),
         )
-
-
+      
 run = pytgcalls.start
